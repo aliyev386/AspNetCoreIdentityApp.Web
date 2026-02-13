4660be58e281bf28b56e1edb585e60f93a1ae78f
aliyev386 <aliyevomer386@gmail.com>
2026-02-13 19:30:55 +0400

    SignUp(Uye ol) Sayfasinin tasarlanmasi

---
+++ AspNetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class SignUpViewModel
+    {
+
+        public SignUpViewModel(string userName, string email, string phone, string password, string passwordConfirm)
+        {
+            UserName = userName;
+            Email = email;
+            Phone = phone;
+            Password = password;
+            PasswordConfirm = passwordConfirm;
+        }
+        [Display(Name ="Kullanici Adi: ")]
+        public string UserName { get; set; }
+        [Display(Name ="Email: ")]
+        public string Email { get; set; }
+        [Display(Name ="Telefon: ")]
+        public string Phone { get; set; }
+        [Display(Name ="Sifre: ")]
+        public string Password { get; set; }
+        [Display(Name ="Sifre tekrar: ")]
+        public string PasswordConfirm { get; set; }
+    }
+}
