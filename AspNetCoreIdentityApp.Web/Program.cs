dbb9d9159a10f08063fd9938feace1e5b15de3ae
aliyev386 <aliyevomer386@gmail.com>
2026-02-13 23:29:03 +0400

    UserList Sayfasinin tasarlanmasi

--- AspNetCoreIdentityApp.Web/Program.cs
+++ AspNetCoreIdentityApp.Web/Program.cs
@@ -32,6 +32,17 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+
+});
+
+
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
